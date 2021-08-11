47247e5f1d2e3e5ca78045c45a17978e63e54cb3
point-do <79358585+point-do@users.noreply.github.com>
2021-08-11 10:25:58 +0900

    Update oric.js

--- js/oric.js
+++ js/oric.js
@@ -1,1 +1,12 @@
-
+$(function(){
+ history.pushState(null, null, null); //ブラウザバック無効化
+ //ブラウザバックボタン押下時
+ $(window).on("popstate", function (event) {
+   history.pushState(null, null, null);
+   window.alert('前のページに戻る場合、前に戻るボタンから戻ってください。');
+ });
+});
+window.addEventListener("beforeunload", function (event) {
+    event.preventDefault();
+    event.returnValue = '';
+  });
